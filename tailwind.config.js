5f0d422bc202c4f5fffdf068080a12413c8b5c0d
reggieleo <153875133+reggieleo@users.noreply.github.com>
2024-10-30 16:27:20 +0300

    start
    smartapp

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+//tailwind.config.js
+module.exports = {
+    theme:{
+        extend:{
+            colors:{
+                'soft-blue': '#A7C6ED',
+                'light-gray': '#F7FAFC',
+                'calm-green': '#B2E0B2',
+                'muted-brown': '#D7C9B5',
+            },
+            fontFamily:{
+                'serif':['Georgia', 'serif'],
+                'sans':['Helvetica', 'Arial', 'sans-serif'],
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
